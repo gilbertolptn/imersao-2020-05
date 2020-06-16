4af81c7063b141494ec913b4f88879a736a6de88
Gilberto Lupatini <gilberto.lupatini@gmail.com>
2020-06-15 22:04:54 -0300

    Exercício Listar Animais

--- Projetos/petshop/src/main/java/br/com/tt/petshop/api/AnimalRestController.java
+++ Projetos/petshop/src/main/java/br/com/tt/petshop/api/AnimalRestController.java
@@ -1,14 +1,11 @@
 package br.com.tt.petshop.api;
 
-
 import br.com.tt.petshop.dto.AnimalEntradaDto;
 import br.com.tt.petshop.model.Animal;
 import br.com.tt.petshop.service.AnimalService;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.net.URI;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -32,4 +29,20 @@ public class AnimalRestController {
         String location = String.format("/clientes/%d/animais/%d", idCliente, animalPersistido.getId());
         return ResponseEntity.created(URI.create(location)).build();
     }
+
+    /**
+     * Listar Todos:
+     *
+     * /clientes/{idCliente}/animais
+     *
+     * - Requisito extra: Filtrar por tipo de animal
+     *
+     * /clientes/{idCliente}/animais?tipo=REPTIL
+     * /clientes/{idCliente}/animais?tipo=MAMIFERO
+     *
+     * Dicas:
+     * - Vai ser apenas um recurso no controller (com param não obrigatório)
+     * - Provavelmente precisará de mais de 1 método no repository....
+     */
+
 }
