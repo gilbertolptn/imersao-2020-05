15afd3c529ccb50b70e5c6a6edfdfd2542e67bd0
Gilberto Lupatini <gilberto.lupatini@gmail.com>
2020-06-29 21:14:29 -0300

    Configuração banco

--- Projetos/petshop/src/main/resources/schema.sql
+++ Projetos/petshop/src/main/resources/schema.sql
@@ -1,16 +1,16 @@
-CREATE TABLE "unidade"(
+CREATE TABLE if not exists "unidade"(
     "id" SERIAL NOT NULL,
     "nome" VARCHAR(100) NOT NULL,
     "endereco" VARCHAR(150) NOT NULL
 );
 --ALTER TABLE "UNIDADE" ADD CONSTRAINT "PK_UNIDADE_ID" PRIMARY KEY("ID");
-CREATE TABLE "tb_cliente"(
+CREATE TABLE if not exists "tb_cliente"(
     "id" SERIAL NOT NULL,
     "nro_cpf" VARCHAR(14) NOT NULL,
     "txt_nome" VARCHAR(255)
 );
 --ALTER TABLE "TB_CLIENTE" ADD CONSTRAINT "PK_CLIENTE_ID" PRIMARY KEY("ID");
-CREATE TABLE "tb_animal"(
+CREATE TABLE if not exists "tb_animal"(
     "id" SERIAL NOT NULL,
     "data_nascimento" date,
     "nome" varchar(255),
@@ -18,7 +18,7 @@ CREATE TABLE "tb_animal"(
     "id_cliente" integer
 );
 --ALTER TABLE "TB_ANIMAL" ADD CONSTRAINT "PK_ANIMAL_ID" PRIMARY KEY("ID");
-CREATE TABLE "tb_atendimento"(
+CREATE TABLE if not exists "tb_atendimento"(
     "id" serial not null,
     "data_hora" timestamp not null,
     "descricao_servico" varchar(255),
@@ -28,5 +28,5 @@ CREATE TABLE "tb_atendimento"(
     "id_animal" bigint not null
 );
 --ALTER TABLE "TB_ATENDIMENTO" ADD CONSTRAINT "PK_ATENDIMENTO_ID" PRIMARY KEY("ID");
-alter table "tb_atendimento" add constraint "fk_cliente_animal" foreign key("id_animal") references "tb_animal"("id");
-alter table "tb_animal" add constraint "fk_animal_cliente" foreign key("id_cliente") references "tb_cliente"("id");
+--alter table "tb_atendimento" add constraint "fk_cliente_animal" foreign key("id_animal") references "tb_animal"("id");
+--alter table "tb_animal" add constraint "fk_animal_cliente" foreign key("id_cliente") references "tb_cliente"("id");
